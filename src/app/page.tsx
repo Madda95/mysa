3be3abd933f55473f3ce878e454d5de0e67663c8
Daniel Maddalena <daniel.maddalena9510@gmail.com>
2025-05-12 10:34:29 +0200

    fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,17 +3,18 @@ import { Footer, Navbar } from "@/components";
 
 // sections
 import Hero from "./hero";
+import RootLayout from "./layout";
 import Projects from "./projects";
 import Resume from "./resume";
 
 export default function Portfolio() {
   return (
-    <>
+    <RootLayout>
       <Navbar />
       <Hero />
       <Resume />
       <Projects />
       <Footer />
-    </>
+    </RootLayout>
   );
 }
